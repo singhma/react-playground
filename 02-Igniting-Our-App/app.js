c6d5b87d0cd9209fba0fe69b4d20dd5459243827
Mannal Singh <mannal.singh@hotmail.com>
2025-07-28 21:17:53 +1000

    before parcel

---
+++ 02-Igniting-Our-App/app.js
@@ -0,0 +1,27 @@
+import React from 'https://esm.sh/react@19.1.0';
+import ReactDOM from 'https://esm.sh/react-dom@19.1.0/client';
+
+
+
+// Build html element using Javascript
+// const heading = document.createElement("h1")
+// heading.innerHTML = "Hello world from Javascript";
+// const root = document.getElementById("root");
+// root.appendChild(headin
+// Build html element using React Element (ESM)
+
+
+// Single React Element
+// const heading = React.createElement("h1", {"id":"heading"}, "Hello world from React Element");
+
+// Nested React Components
+const nestedElements = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "H1 Tag"),
+        React.createElement("h2", {}, "H2 Tag"),
+    ]
+    ));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedElements);

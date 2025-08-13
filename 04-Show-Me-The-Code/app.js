fd496b4f5132c088c0a5ec50aa6a9a772034a796
Mannal Singh <mannal.singh@hotmail.com>
2025-08-13 21:01:19 +1000

    added app layout template

---
+++ 04-Show-Me-The-Code/app.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Header
+ *  Logo
+ *  Nav Items
+ * Body
+ *  Search
+ *  Restaurant cards
+ *    Dishname
+ *    Image
+ *    Restaurant Name
+ *    Rating
+ *    Cuisines
+ *    Time to Deliver
+ * Footer
+ *  Copywright
+ *  Links
+ *  Address
+ *  Contact
+ * 
+ */
